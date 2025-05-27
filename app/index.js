059b28470ad6e574edf6846bbe16dd019763c8f9
ogunmehmetsalih <ogunmehmetsalih@gmail.com>
2025-05-27 18:48:18 +0300

    CI/CD file is added

---
+++ app/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const redis = require("redis");
+
+const app = express();
+const client = redis.createClient({ url: "redis://redis:6379" });
+
+client.connect().catch(console.error);
+
+app.get("/", async (req, res) => {
+  let visits = await client.get("visits");
+  visits = visits ? parseInt(visits) + 1 : 1;
+  await client.set("visits", visits);
+  res.send(`Ziyaret sayısı: ${visits}`);
+});
+
+app.listen(3000, () => {
+  console.log("Web app çalışıyor: http://localhost:3000");
+});
+
