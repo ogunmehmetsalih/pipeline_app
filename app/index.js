f72f845d5b3c75814dfcbe8a9a005e5d8ebd67b8
m4rul <111509452+ogunmehmetsalih@users.noreply.github.com>
2025-05-27 19:10:29 +0300

    Update index.js

--- app/index.js
+++ app/index.js
@@ -10,7 +10,7 @@ app.get("/", async (req, res) => {
   let visits = await client.get("visits");
   visits = visits ? parseInt(visits) + 1 : 1;
   await client.set("visits", visits);
-  res.send(`Ziyaret sayısı: ${visits}`);
+  res.send(`Ziyaret sayısı: ${visits} — bu güncelleme CI/CD ile geldi!`);
 });
 
 app.listen(3000, () => {
